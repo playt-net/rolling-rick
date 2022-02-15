2e50d76045957bdaece6dc8d3b3d388043a2f34e
lmachens <leon@machens.koeln>
2022-02-15 11:06:10 +0100

    ✨ Add game over video

--- game/game.js
+++ game/game.js
@@ -187,4 +187,8 @@ function hitBomb(player, bomb) {
 
   gameOver = true;
   submitScore(score);
+
+  const div = document.createElement("div");
+  div.innerHTML = `<iframe class="rick" width="560" height="315" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.body.append(div);
 }

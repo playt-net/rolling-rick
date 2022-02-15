1fa05e2f4b29d5630f1c9520d4e6851453cd451f
lmachens <leon@machens.koeln>
2022-02-15 12:00:13 +0100

    ✨ Submit score after 3 sec

--- game/game.js
+++ game/game.js
@@ -186,9 +186,12 @@ function hitBomb(player, bomb) {
   player.anims.play("turn");
 
   gameOver = true;
-  submitScore(score);
 
   const div = document.createElement("div");
   div.innerHTML = `<iframe class="rick" width="560" height="315" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   document.body.append(div);
+
+  setTimeout(() => {
+    submitScore(score);
+  }, 3000);
 }

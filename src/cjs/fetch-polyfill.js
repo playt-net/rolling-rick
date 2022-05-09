ed0e03b8cc70e98e995000038997cfb782e6004b
lmachens <leon@machens.koeln>
2022-05-09 12:45:21 +0200

    🐛 Always load node-fetch polyfill Due to an issue in Node.js 18.1.0 with fetch implementation of undici, the request content-type was invalid. See https://github.com/nodejs/undici/pull/1401 This change makes sure to always use stable node-fetch.

--- src/cjs/fetch-polyfill.js
+++ src/cjs/fetch-polyfill.js
@@ -1,8 +1,6 @@
 const { default: fetch, Headers, Request, Response } = require("node-fetch");
 
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch;
-  globalThis.Headers = Headers;
-  globalThis.Request = Request;
-  globalThis.Response = Response;
-}
+globalThis.fetch = fetch;
+globalThis.Headers = Headers;
+globalThis.Request = Request;
+globalThis.Response = Response;
--- src/ts/fetch-polyfill.ts
+++ src/ts/fetch-polyfill.ts
@@ -1,9 +1,7 @@
 // @ts-nocheck
 import fetch, { Headers, Request, Response } from "node-fetch";
 
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch as any;
-  globalThis.Headers = Headers as any;
-  globalThis.Request = Request as any;
-  globalThis.Response = Response as any;
-}
+globalThis.fetch = fetch as any;
+globalThis.Headers = Headers as any;
+globalThis.Request = Request as any;
+globalThis.Response = Response as any;

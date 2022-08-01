0832c2cecf4dddb0611772b159117bba9abc8db9
lmachens <leon@machens.koeln>
2022-08-01 10:41:26 +0200

    🔨 Add webpack to bundle modules

---
+++ webpack.config.cjs
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/game/game.ts",
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.game.json",
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "game.js",
+    path: path.resolve(__dirname, "game"),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.API_HOST": JSON.stringify(process.env.API_HOST),
+    }),
+  ],
+};

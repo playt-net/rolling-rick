bfd490712dacb5bbbe25a8777b9dd36f8d49c774
Tim Seckinger <seckinger.tim@gmail.com>
2023-09-29 14:38:40 +0200

    fml

--- api/index.mjs
+++ api/index.mjs
@@ -1,13 +1,3 @@
 import app from "../dist/src/server/app.mjs";
-import { readFileSync, readdirSync } from "node:fs";
-
-console.log(readdirSync("/var/task"));
-console.log(readdirSync("/var/task/node_modules"));
-console.log(readdirSync("/var/task/node_modules/@playt"));
-console.log(readdirSync("/var/task/node_modules/@playt/client"));
-console.log(readdirSync("/var/task/node_modules/@playt/client/dist"));
-console.log(
-  readFileSync("/var/task/node_modules/@playt/client/package.json", "utf8")
-);
 
 export default app;

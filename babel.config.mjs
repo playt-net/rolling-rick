e6ff4270e9092a6f9d7ae73f2aa781a96f91581b
Tim Seckinger <seckinger.tim@gmail.com>
2023-12-06 13:20:47 +0100

    🚑 fix missing game version
    idk why this works with local `npm start` but not on the deploy

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,4 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
   presets: ["@babel/preset-typescript"],
+  plugins: ['transform-inline-environment-variables'],
 };
 export default config;

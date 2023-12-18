8b6ce8b85aae9ab02c18386214fdd458859ba550
Tim Seckinger <seckinger.tim@gmail.com>
2023-12-18 12:18:54 +0100

    🐛 only inline environment variables where absolutely necessary (#81)
    so that we don't accidentally inline ones missing at build time as `undefined`

--- babel.config.mjs
+++ babel.config.mjs
@@ -3,6 +3,6 @@ dotenv.config();
 
 const config = {
   presets: ["@babel/preset-typescript"],
-  plugins: ['transform-inline-environment-variables'],
+  plugins: [['transform-inline-environment-variables', {include: ['npm_package_version']}]],
 };
 export default config;
